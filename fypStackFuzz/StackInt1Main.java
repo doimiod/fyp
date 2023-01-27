f38886b0f31fff050240c43138907f6afd94481b
Masanari Doi <91431813+doimiod@users.noreply.github.com>
2023-01-27 07:52:09 +0000

    check if it really avoid the same num in the stack

--- fypStackFuzz/StackInt1Main.java
+++ fypStackFuzz/StackInt1Main.java
@@ -3,8 +3,8 @@ public class StackInt1Main {
 	public static void main(String[] args) {
 		
 		StackInt1 s1 = new StackInt1(5);
-		StackInt1 s2 = new StackInt1(8);
-		StackInt1 s3 = new StackInt1(0);
+		// StackInt1 s2 = new StackInt1(8);
+		// StackInt1 s3 = new StackInt1(0);
 		
 		// s3.push(1);
 		// s3.pop();
@@ -15,34 +15,47 @@ public class StackInt1Main {
 		s1.push(1);
 		s1.push(2);
 		s1.push(3);
-		s1.push(4);
-		s1.pop();
-		s1.pop();
-		s1.pop();
-		s1.pop();
+		s1.push(3);
+		s1.push(5);
 		s1.push(1);
 		s1.push(2);
+		s1.push(3);
+		s1.push(1);
+		s1.push(2);
+		s1.push(3);
+		s1.push(4);
+		// s1.push(5);
+		// s1.push(4);
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		// s1.push(1);
+		// s1.push(2);
 		
-		s1.peek();
+// 		s1.peek();
 		
-//		System.out.println(s1.peek());
-		s2.peek();
-		s2.push(1);
-		s2.push(2);
-		s2.push(3);
-		s2.push(4);
-		s2.push(5);
-		s2.push(6);
-		s2.push(7);
-		s2.push(8);
-		s2.push(9);
-		s2.pop();
-		s2.pop();
-		s2.pop();
-		s2.pop();
+// //		System.out.println(s1.peek());
+// 		s2.peek();
+// 		s2.push(1);
+// 		s2.push(2);
+// 		s2.push(3);
+// 		s2.push(4);
+// 		s2.push(5);
+// 		s2.push(6);
+// 		s2.push(7);
+// 		s2.push(8);
+// 		s2.push(9);
+// 		s2.pop();
+// 		s2.pop();
+// 		s2.pop();
+// 		s2.pop();
 		
-		s1.peek();
-		s2.peek();
+// 		s1.peek();
+// 		s2.peek();
 		
 //		System.out.println(s1.peek());
 //		System.out.println(s2.peek());
